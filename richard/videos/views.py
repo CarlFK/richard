b762a6757f54a5b7233fed9ef33c96e4f9f8d74f
Carl Karsten <carl@personnelware.com>
2014-06-21 21:15:55 -0500

    now with speaker names.

--- richard/videos/views.py
+++ richard/videos/views.py
@@ -21,6 +21,8 @@ from django.db.models import Count
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core.paginator import EmptyPage, Paginator
+from django.forms.formsets import formset_factory
+from django.forms.models import modelformset_factory
 from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext
@@ -163,17 +165,29 @@ def video(request, video_id, slug):
     if "editkey" in request.GET:
         edit_key = request.GET['editkey']
         template='videos/video_edit.html'
+
+        Speaker_FormSet = modelformset_factory(
+                models.Speaker,
+                extra=0,
+                fields=('name',) )
+
         if request.method == 'POST':
+
             video_form = forms.Video_Form(request.POST, instance=obj)
-            if video_form.is_valid():
+            speaker_formset = Speaker_FormSet(
+                    request.POST, queryset=obj.speakers.all())
+
+            if video_form.is_valid() and speaker_formset.is_valid():
                 video_form.save()
-            else:
-                print video_form.errors
+                speaker_formset.save()
+
         else:
             video_form = forms.Video_Form(instance=obj)
+            speaker_formset = Speaker_FormSet(queryset=obj.speakers.all())
     else:
         template='videos/video.html'
         video_form = None
+        speaker_formset = None
 
     ret = render(
         request,
@@ -181,6 +195,7 @@ def video(request, video_id, slug):
         {'meta': meta,
          'v': obj,
          'v_form':video_form,
+         's_form':speaker_formset,
          'embed': embed,
          'embed_type': embed_type,
          'available_formats': available_formats},
